e84421987b0f60d600b662fcc2858e8118f60fee
bimsara2003 <wijewanthabimsara@gmail.com>
2026-04-06 14:55:18 +0530

    created User model partially with schema and password hashing middleware

---
+++ server/models/User.Model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@my\.campus\.lk$/,
+        "Please add a valid email address",
+      ],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
+      trim: true,
+    },
+    role: {
+      type: [String],
+      enum: ["student", "staff", "admin", "rep"],
+      default: ["student"],
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
