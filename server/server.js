78f0135fb64d81285fd819f8453497be3ee160f2
bimsara2003 <wijewanthabimsara@gmail.com>
2026-04-15 20:44:10 +0530

    feat: Add user routes for handling user-related API requests

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./config/database");
+const userRoutes = require("./routes/userRoute");
 const PORT = process.env.PORT;
 
 const app = express();
@@ -11,6 +12,8 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
