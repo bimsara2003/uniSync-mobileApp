a35475c61fdd87ce090ef508d3cb3cfb80a9e8f2
bimsara2003 <wijewanthabimsara@gmail.com>
2026-04-16 09:34:14 +0530

    feat: Enhance auth middleware to include admin role check and improve error handling

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -11,23 +11,27 @@ const protect = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
 
-      const decoded = jwt.verify(
-        token,
-        process.env.JWT_SECRET || "fallback_secret_key",
-      );
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
-
-      next();
+      return next();
     } catch (error) {
       console.error(error);
-      res.status(401).json({ message: "Not authorized, token failed" });
+      return res.status(401).json({ message: "Not authorized, token failed" });
     }
   }
 
   if (!token) {
-    res.status(401).json({ message: "You are not authorized" });
+    return res.status(401).json({ message: "You are not authorized" });
   }
 };
 
-module.exports = { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.role.includes("ADMIN")) {
+    next();
+  } else {
+    res.status(403).json({ message: "Not authorized as an admin" });
+  }
+};
+
+module.exports = { protect, admin };
