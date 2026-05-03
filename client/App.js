ae7c076981f082ca02123ba990f633438d8343b2
bimsara2003 <wijewanthabimsara@gmail.com>
2026-05-03 07:37:45 +0530

    style: fix whitespace in App.js

--- client/App.js
+++ client/App.js
@@ -1,5 +1,5 @@
 import { AuthProvider } from "./src/context/AuthContext";
-import AppNavigator    from "./src/navigation/AppNavigator";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
