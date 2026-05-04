1da518f7d545277a1667a4d6697699d696b9f367
bimsara2003 <wijewanthabimsara@gmail.com>
2026-05-04 12:20:51 +0530

    feat: add admin API client (getAllUsers, deactivate, reactivate, updateRole, delete)

---
+++ client/src/api/admin.js
@@ -0,0 +1,9 @@
+import axiosInstance from "./client";
+
+export const adminAPI = {
+  getAllUsers:    ()           => axiosInstance.get("/admin/users"),
+  deactivateUser:(id)         => axiosInstance.put(`/admin/users/${id}/deactivate`),
+  reactivateUser:(id)         => axiosInstance.put(`/admin/users/${id}/reactivate`),
+  updateUserRole:(id, role)   => axiosInstance.put(`/admin/users/${id}/role`, { role }),
+  deleteUser:    (id)         => axiosInstance.delete(`/admin/users/${id}`),
+};
