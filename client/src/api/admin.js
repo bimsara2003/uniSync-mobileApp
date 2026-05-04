a436beb0f3a93c2854d44f834043a64a65515dcd
bimsara2003 <wijewanthabimsara@gmail.com>
2026-05-04 12:59:40 +0530

    fix: correct import path from ./client to ./axiosInstance in admin.js

--- client/src/api/admin.js
+++ client/src/api/admin.js
@@ -1,9 +1,10 @@
-import axiosInstance from "./client";
+import axiosInstance from "./axiosInstance";
 
 export const adminAPI = {
-  getAllUsers:    ()           => axiosInstance.get("/admin/users"),
-  deactivateUser:(id)         => axiosInstance.put(`/admin/users/${id}/deactivate`),
-  reactivateUser:(id)         => axiosInstance.put(`/admin/users/${id}/reactivate`),
-  updateUserRole:(id, role)   => axiosInstance.put(`/admin/users/${id}/role`, { role }),
-  deleteUser:    (id)         => axiosInstance.delete(`/admin/users/${id}`),
+  getAllUsers: () => axiosInstance.get("/admin/users"),
+  deactivateUser: (id) => axiosInstance.put(`/admin/users/${id}/deactivate`),
+  reactivateUser: (id) => axiosInstance.put(`/admin/users/${id}/reactivate`),
+  updateUserRole: (id, role) =>
+    axiosInstance.put(`/admin/users/${id}/role`, { role }),
+  deleteUser: (id) => axiosInstance.delete(`/admin/users/${id}`),
 };
